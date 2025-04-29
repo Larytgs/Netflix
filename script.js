2285f9a11c042726d74895439adf5d827b655f43
laryTgs <laryssatortato99@gmail.com>
2025-04-29 15:24:38 -0300

    up

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function sair(elemento){
-    var lista = document.getElementById(elemento).style.lista;
-    if (lista == 'block'){
-        document.getElementById(elemento).style.lista = 'none';
-    }else{
-        document.getElementById(elemento).style.lista = 'block';
-    }
+function sair(elemento) {
+  var lista = document.getElementById(elemento).style.lista;
+  if (lista == "block") {
+    document.getElementById(elemento).style.lista = "none";
+  } else {
+    document.getElementById(elemento).style.lista = "block";
+  }
 }
+
+// header
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
